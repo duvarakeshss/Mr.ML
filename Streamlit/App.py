925ee92e793205b9bc234621a7c1ec23fb9f46f1
duvarakesh <duvarakesh05@gmail.com>
2024-10-28 16:32:46 +0530

    commit

--- Streamlit/App.py
+++ Streamlit/App.py
@@ -7,7 +7,7 @@ with col1:
   
 # Sidebar options
 st.sidebar.title("Select Model")
-model_type = st.sidebar.radio("Choose a model type:", ("Regression", "Classification", "Clustering","Tuberculosis"))
+model_type = st.sidebar.radio("Choose a model type:", ("Regression", "Classification", "Clustering"))
 
 
 if model_type == "Regression":
@@ -65,6 +65,4 @@ elif model_type == "Clustering":
     if clustering_option == "Spectral":
         from spectral import main
         main()
-elif model_type == "Tuberculosis":
-    from Predict import main
-    main()
+
